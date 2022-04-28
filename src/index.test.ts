e4e3ae28774fc8c95d0ba0ad0942ac5236c88854
Sairo Sabater <heroshinsoul@gmail.com>
2022-04-28 18:35:32 +0200

    scores two and three consecutive strikes knocking down all the pins in the first try

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,9 +5,9 @@ describe("BowlingGame", () => {
     const initialScore = 0;
     let bowlingGame = new BowlingGame()
 
-    const score = bowlingGame.actualScore();
+    const actualScore = bowlingGame.actualScore();
 
-    expect(score).toEqual(initialScore);
+    expect(actualScore).toEqual(initialScore);
   })
   it("throws two balls without knocking down all the pins and returns the sum of both", () => {
     const turnScore = 9;
@@ -16,9 +16,9 @@ describe("BowlingGame", () => {
     let bowlingGame = new BowlingGame()
     bowlingGame.turnScores(firstTryBall, secondTryBall)
 
-    const score = bowlingGame.actualScore();
+    const actualScore = bowlingGame.actualScore();
 
-    expect(score).toEqual(turnScore);
+    expect(actualScore).toEqual(turnScore);
   })
   it("throws four balls in two turns without knocking down all the pins and returns the total sum", () => {
     const totalScore = 16;
@@ -26,9 +26,30 @@ describe("BowlingGame", () => {
     bowlingGame.turnScores(9, 0)
     bowlingGame.turnScores(3, 4)
 
+    const actualScore = bowlingGame.actualScore();
+
+    expect(actualScore).toEqual(totalScore);
+  })
+  it("scores two consecutive strikes knocking down all the pins in the first try", () => {
+    const totalScore = 30;
+    let bowlingGame = new BowlingGame()
+    bowlingGame.turnScores(10, 0)
+    bowlingGame.turnScores(10, 0)
+
     const score = bowlingGame.actualScore();
 
     expect(score).toEqual(totalScore);
   })
+  it("scores three consecutive strikes knocking down all the pins in the first try", () => {
+    const totalScore = 60;
+    let bowlingGame = new BowlingGame()
+    bowlingGame.turnScores(10, 0)
+    bowlingGame.turnScores(10, 0)
+    bowlingGame.turnScores(10, 0)
 
+    const score = bowlingGame.actualScore();
+
+    expect(score).toEqual(totalScore);
+  })
+  
 })
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 export class BowlingGame {
   score = 0
+  strike = 0
+  strikeScore = 0
   actualScore(): number {
     return this.score
   }
   turnScores(firstTryBall: number, secondTryBall: number) {
-    this.score += firstTryBall + secondTryBall
+    if (this.strike > 0){
+      if (firstTryBall === 10){
+        this.strikeScore += firstTryBall
+        this.strike -1
+      }
+      else {
+        this.strikeScore += firstTryBall + secondTryBall
+        this.strike - 2
+      }
+    }
+    if (firstTryBall === 10){
+      this.strike += 2
+    }
+    this.score += firstTryBall + secondTryBall + this.strikeScore
   }
 }
