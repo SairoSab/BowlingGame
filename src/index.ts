905763c64005a1307f86433c117e549c8678ad98
Sairo Sabater <heroshinsoul@gmail.com>
2022-04-30 12:55:24 +0200

    Revert "commit de prueba2"
    This reverts commit 3dbaf9f6968eba5bde37dcdc4fb27df4dee87fef.

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ export class BowlingGame {
   score = 0
   strike = 0
   strikeScore = 0
-  turn : number = 1
+  turn = 1
   detailedTurnPuntuation = {
-    1 : 0,
-    2 : 0,
-    3 : 0,
-    4 : 0,
-    5 : 0,
-    6 : 0,
-    7 : 0,
-    8 : 0,
-    9 : 0,
-    10 : 0
+    "1" : 0,
+    "2" : 0,
+    "3" : 0,
+    "4" : 0,
+    "5" : 0,
+    "6" : 0,
+    "7" : 0,
+    "8" : 0,
+    "9" : 0,
+    "10" : 0
   }
   throwsToSum = {
     1 : 0,
